f94e22b97d6c55fa30e10dbb5a7cd79253a95ebb
Rasalghul <rajrohan.thakur07@gmail.com>
2026-04-23 05:12:28 -0500

    feat: initialize project structure and dependencies

---
+++ src/kal_predict/__init__.py
@@ -0,0 +1,8 @@
+"""Kal..Predict: autonomous trading agent for Kalshi prediction markets."""
+
+__version__ = "0.1.0"
+__author__ = "Rasalghul"
+
+from . import config, logging_setup, trace
+
+__all__ = ["config", "logging_setup", "trace"]
